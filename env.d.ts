9c6dbbd8cbe169e71eb657eec28809e7a7843c5d
Vitalii Dudnik <dudnikvtl@gmail.com>
2025-12-04 20:18:18 +0200

    fix: add type declarations for Vueless i18n module and update TypeScript configuration to include ES2021 library

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+declare module "virtual:vueless/vue-i18n" {
+  import type { I18n } from "vue-i18n";
+  export type { I18n };
+  export type VueMessageType = Record<string, any>;
+}
